a590acd4944bb324fe2c63369d005a60d2568f78
Darshan9221 <darshankumar.patel@zeuslearning.com>
2025-06-12 19:28:56 +0530

    Started with Task 5

---
+++ Darshan_Patel_Task_5/Dashboard/index.js
@@ -0,0 +1,9 @@
+export const menuItems = [
+  { label: "Dashboard", badge: 0 }, // Replace emoji with actual icon component if using
+  { label: "Content", badge: 0 },
+  { label: "Users", icon: "👥", badge: 0 },
+  { label: "Reports", icon: "📊", badge: 0 },
+  { label: "Admin", icon: "🛠️", badge: 0 },
+  { label: "Notifications", icon: "🔔", badge: 2 },
+  { label: "Announcements", icon: "📢", badge: 1 },
+];
